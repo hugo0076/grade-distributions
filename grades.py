6ac20627c43e1121ee4f7014f687b6e7126bcd5e
Hugo Lyons Keenan <hugo0076@gmail.com>
2023-11-25 11:54:04 +1100

    uploading, score aggregation

--- grades.py
+++ grades.py
@@ -1,45 +1,154 @@
+import io
 import dash
-from dash import dcc, html, dash_table, Input, Output, State
+import dash_bootstrap_components as dbc
+from dash import dcc, html, dash_table, Input, Output, State, Dash
+import plotly.graph_objs as go
 import pandas as pd
+import base64
+import fitz  # PyMuPDF
+from util import extract_subject_data, store_subject_data, read_subject_data
 
-
-app = dash.Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 server = app.server
 
-app.layout = html.Div([
-    dcc.Input(id='input-field', type='text', placeholder='Enter text here...'),
-    html.Button('Save', id='save-button', n_clicks=0),
-    dash_table.DataTable(id='table')
-])
+app.layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.Div(
+                            [
+                                dcc.Upload(
+                                    id="upload-data",
+                                    children=html.Div(
+                                        ["Upload Transcript", html.A(" (Select Files)")]
+                                    ),
+                                    style={
+                                        "width": "100%",
+                                        "height": "60px",
+                                        "lineHeight": "60px",
+                                        "borderWidth": "1px",
+                                        "borderStyle": "dashed",
+                                        "borderRadius": "5px",
+                                        "textAlign": "center",
+                                        "margin": "10px",
+                                    },
+                                    multiple=False,
+                                )
+                            ],
+                            style={"width": "75%", "margin": "auto", "align": "center"},
+                        )
+                    ],
+                    width=6,
+                ),
+                dbc.Col(
+                    [
+                        html.Div(
+                            [
+                                dcc.Dropdown(
+                                    [],
+                                    id="subject-year-dropdown",
+                                    placeholder="Select a subject",
+                                    style={"backgroundColor": "#888", "color": "#000"},
+                                ),
+                                html.Div(id="dd-output-container"),
+                            ],
+                            style={"width": "75%", "margin": "auto", "align": "center"},
+                        )
+                    ],
+                    width=6,
+                    className="text-right",
+                ),
+            ],
+            align="center",
+            justify="center",
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.Div(
+                            [dcc.Graph(id="main-graph", figure=go.Figure())],
+                            style={
+                                "width": "500px",
+                                "margin": "auto",
+                                "align": "center",
+                            },
+                        )
+                    ],
+                    width=12,
+                    className="mx-auto p-4",
+                )
+            ],
+            align="center",
+            justify="center",
+        ),
+        dbc.Modal(
+            [
+                dbc.ModalHeader("File Upload"),
+                dbc.ModalBody(id="modal-body"),
+                dbc.ModalFooter(dbc.Button("Close", id="close", className="ml-auto")),
+            ],
+            id="modal",
+        ),
+        html.Div(id="hidden-div", style={"display": "none"}),
+    ],
+    fluid=True,
+)
+
 
 @app.callback(
-    Output('input-field', 'value'),
-    Input('save-button', 'n_clicks'),
-    State('input-field', 'value')
+    Output("hidden-div", "children"),
+    Output("subject-year-dropdown", "options"),
+    Input("upload-data", "contents"),
+    State("upload-data", "filename"),
 )
-def save_to_csv(n_clicks, value):
-    if n_clicks > 0 and value is not None:
-        df = pd.DataFrame([value])
-        df.to_csv('file.csv', mode='a', header=False, index=False)
-        return ''
+def read_file(contents, filename):
+    """
+    Reads the uploaded file and extracts the subject data. Stores the subject data in a CSV file.
+    """
+    if contents is not None:
+        content_type, content_string = contents.split(",")
+        if "pdf" not in content_type:
+            print("File not PDF")
+            return dash.no_update, dash.no_update
+        decoded = base64.b64decode(content_string)
+        pdf_file = io.BytesIO(decoded)
+        doc = fitz.open("pdf", pdf_file.read())
+        extracted_text = "".join([page.get_text() for page in doc])
+        doc.close()
+        records = extract_subject_data(extracted_text)
+        num_records = len(records)
+        print(num_records)
+        print(store_subject_data(records))
+        data, unique_subject_years = read_subject_data()
+        return num_records, list(unique_subject_years.keys())
     else:
-        return value
+        return 0, dash.no_update
+
 
 @app.callback(
-    Output('table', 'data'),
-    Output('table', 'columns'),
-    Input('save-button', 'n_clicks')
+    Output("modal", "is_open"),
+    Output("modal-body", "children"),
+    Input("upload-data", "contents"),
+    Input("close", "n_clicks"),
+    Input("hidden-div", "children"),
+    State("upload-data", "filename"),
+    prevent_initial_call=True,
 )
-def update_table(n_clicks):
-    if n_clicks > 0:
-        print('update table')
-        df = pd.read_csv('file.csv', header=None)
-        data = df.to_dict('records')
-        columns = [{"name": str(i), "id": str(i)} for i in df.columns]
-        return data, columns
+def toggle_modal(contents, n_clicks, num_records, filename):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        return False, ""
     else:
-        print('no update')
-        return [], []
+        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+        if button_id == "upload-data" and contents is not None:
+            message = f"File {filename} uploaded successfully. {num_records} records were extracted."
+            return True, message
+        else:
+            return False, ""
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run_server(debug=True)
---
+++ util.py
@@ -0,0 +1,83 @@
+import fitz  # PyMuPDF
+import re
+import csv
+import pandas as pd
+
+# Regex pattern to match year and following text
+YEAR_PATTERN = r"(\b\d{4}\b.*?)(?=\b\d{4}\b|$)"
+SCORE_PATTERN = r"\b([A-Z]+\d+)\s+[A-Z0-9]+\^?\s+([^\n]+)\n(\d{2,3})"
+
+
+def extract_subject_data(extracted_text):
+    """
+    Extracts subject data from the extracted text. Returns a list of tuples of the form:
+    (subject_code, subject_name, grade, score, year)
+    """
+    matches = re.findall(YEAR_PATTERN, extracted_text, re.DOTALL)
+    all_matches = []
+    for match in matches:
+        year = match[:4]
+        score_pattern = re.compile(SCORE_PATTERN)
+        score_matches = score_pattern.findall(match)
+        score_matches = [(*match, int(year)) for match in score_matches]
+        all_matches.extend(score_matches)
+    return all_matches
+
+
+def store_subject_data(subject_data):
+    """
+    Appends the subject data to a CSV file.
+    """
+    if not subject_data:
+        return False
+    with open("all_scores.csv", "a", newline="") as f:
+        writer = csv.writer(f)
+        for data in subject_data:
+            writer.writerow(data)
+            # f.write(','.join([str(x) for x in data]) + '\n')
+    return True
+
+
+def read_subject_data(fn="all_scores.csv"):
+    """
+    Reads the subject data from the CSV file. Returns a dataframe.
+    """
+    df = pd.read_csv(
+        fn, header=None, names=["subject_code", "subject_name", "score", "year"]
+    )
+    # cast year and score to int
+    df["year"] = df["year"].astype(int)
+    df["score"] = df["score"].astype(int)
+
+    # get unique subject-year combinations
+    df_duplicates = (
+        df.groupby(["subject_code", "subject_name", "year"])
+        .size()
+        .reset_index(name="count")
+    )
+    print(df_duplicates)
+    # get a list of strings of the form '(year) subject_code - subject_name (count)'
+    subject_year_dict = df_duplicates.apply(
+        lambda row: {
+            f"({row['year']}) {row['subject_code']} - {row['subject_name']} (n={row['count']})": (
+                row["year"],
+                row["subject_code"],
+                row["subject_name"],
+            )
+        },
+        axis=1,
+    ).to_dict()
+    # convert the dict from {n:{str:(year, subject_code, subject_name)}} to {str:(year, subject_code, subject_name)}
+    subject_year_dict = {k: v for d in subject_year_dict.values() for k, v in d.items()}
+    return df, subject_year_dict
+
+
+if __name__ == "__main__":
+    # Open the uploaded PDF file
+    file_path = "./StatementofResults-1081696-24_Nov_2023.pdf"
+    doc = fitz.open(file_path)
+    # Extract text from each page
+    extracted_text = "".join([page.get_text() for page in doc])
+    # Close the document
+    doc.close()
+    print(extract_subject_data(extracted_text))
